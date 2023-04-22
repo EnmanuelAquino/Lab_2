48f73cfb809dbcffcf3192d9dfb5f9ff0705a6d1
EnmanuelAquino <126352152+EnmanuelAquino@users.noreply.github.com>
2023-04-22 17:17:06 -0400

    Add files via upload

---
+++ part3/test.sh
@@ -0,0 +1,24 @@
+source ../../env.sh
+../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part3/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part3/output
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /part3/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal nyc_parking_violations_data.csv /part3/input/
+
+
+echo "--------------------------- Parallelism 2---------------------------" 
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 hour_car.py hdfs://$SPARK_MASTER:9000/part3/input/ --conf spark.default.parallelism=2
+echo "--------------------------- Parallelism 3---------------------------" 
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 hour_car.py hdfs://$SPARK_MASTER:9000/part3/input/ --conf spark.default.parallelism=3
+echo "--------------------------- Parallelism 4---------------------------" 
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 hour_car.py hdfs://$SPARK_MASTER:9000/part3/input/ --conf spark.default.parallelism=4
+echo "--------------------------- Parallelism 5---------------------------" 
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 hour_car.py hdfs://$SPARK_MASTER:9000/part3/input/ --conf spark.default.parallelism=5
+
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part3/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part3/output/
+../stop.sh
+
+
+
+
