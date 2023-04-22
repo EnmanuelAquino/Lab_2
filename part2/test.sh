bf492171ac0497c91a284bab3e75e3976d64b57b
EnmanuelAquino <126352152+EnmanuelAquino@users.noreply.github.com>
2023-04-22 17:16:22 -0400

    Add files via upload

---
+++ part2/test.sh
@@ -0,0 +1,12 @@
+source ../../env.sh
+../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /part2/output
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /part2/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal nyc_parking_violations_data.csv /part2/input/
+
+echo "--------------------------- Question: What is the probability that it will get an ticket?? ---------------------------" 
+
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./Black_Ticket.py hdfs://$SPARK_MASTER:9000/part2/input/
+
+../stop.sh
